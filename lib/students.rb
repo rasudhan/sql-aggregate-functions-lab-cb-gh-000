0f6bf7ac9e0b9abe41839baa404e299b07a79c51
Anandha Sudhan <rasudhan@gmail.com>
2018-10-21 19:08:04 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,13 +5,18 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) FROM STUDENTS;"
 end
 
 def average_student_gpa
+   "SELECT AVG(GPA) FROM STUDENTS;"
 end
 
 def total_tardies_for_all_students
+  
+ "SELECT SUM(tardies) FROM STUDENTS;"
 end
 
 def average_gpa_for_9th_grade
+   "SELECT AVG(gpa) FROM STUDENTS WHERE grade='9';"
 end
